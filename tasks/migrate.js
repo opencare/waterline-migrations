15fc01e2e56b8dd2d12ebfc8d9e5225748bd3242
Cameron Howieson <cameron.howieson@gmail.com>
2014-03-19 17:01:10 -0400

    Working template creation

--- tasks/migrate.js
+++ tasks/migrate.js
@@ -5,39 +5,56 @@ var SailsIntegration = require('../lib/sailsIntegration'),
   Migrator = require('../lib/migrator');
 
 module.exports = function(grunt) {
-  grunt.registerTask("migrate", "Migrate Waterline models", function(action) {
+  grunt.registerTask("migrate", "Migrate Waterline models", function(action, name) {
 
     var done = this.async();
     var baseAppPath = process.cwd();
     var modulesPath = path.join(baseAppPath, 'node_modules');
-
-    console.log(baseAppPath);
+    var migrationsPath = path.join(baseAppPath, 'db', 'migrations')
 
     // Check if we're migrating or creating new
-    if (action == 'create') {
-      grunt.log.writeln('Creating new migration...');
-      done();
-    } else if (action == 'up' || action == 'down') {
-      grunt.log.writeln('Migrating database...');
+    switch (action) {
+      case 'create':
+        if (!name) {
+          grunt.log.error('Please specify a name for this migration using migrate:create:{name}.');
+          return done(false);
+        }
+        grunt.log.writeln('Creating new migration...');
 
-      SailsIntegration.loadSailsConfig(modulesPath, function(err, sailsConfig) {
-        if (err) return done(err);
+        var timestamp = Date.now();
+        var template = path.join(__dirname, '../lib/templates/migrationTemplate.js');
+        var newFilename = timestamp + '_' + name + '.js';
+        grunt.file.copy(template, path.join(migrationsPath, newFilename));
 
-        var config = {
-          adapter: sailsConfig.defaultAdapter,
-          migrationsDir: path.join(baseAppPath, 'db', 'migrations')
-        };
-        
-        Migrator.migrate(config, action, function(err) {
-          if (err) return done(false);
+        grunt.log.writeln('Migration ' + newFilename + ' successfully created.');
 
-          grunt.log.writeln('Migration complete.');
-          done();
+        done();
+        return;
+
+      case 'up':
+      case 'down':
+        grunt.log.writeln('Migrating database...');
+
+        SailsIntegration.loadSailsConfig(modulesPath, function(err, sailsConfig) {
+          if (err) return done(err);
+
+          var config = {
+            adapter: sailsConfig.defaultAdapter,
+            migrationsDir: migrationsPath
+          };
+          
+          Migrator.migrate(config, action, function(err) {
+            if (err) return done(false);
+
+            grunt.log.writeln('Migration complete.');
+            done();
+          });
         });
-      });
-    } else {
-      grunt.log.error("Unknown command. Please use one of 'migrate:create', 'migrate:up', or 'migrate:down'.");
-      done(false);
+        return;
+
+      default:
+        grunt.log.error("Unknown command. Please use one of 'migrate:up', 'migrate:down', or 'migrate:create:{name}'.");
+        return done(false);
     }
   });
 };
